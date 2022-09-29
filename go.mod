4637e23285dc15c99bf03db1c2d9618084f4c4e0
Diego Rodríguez Rodríguez <diegodelemos@pm.me>
2022-09-29 17:38:48 +0200

    Move to Hugo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/diegodelemos/diegodelemos.github.io
+
+go 1.17
--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "../styles/global.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the CSS
-config.autoAddCss = false;
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
