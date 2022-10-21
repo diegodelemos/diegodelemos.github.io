fc1c9c863b8a0fc2520ad5fa55b26aafd81a36b6
Diego Rodríguez Rodríguez <diegodelemos@pm.me>
2022-10-21 16:55:30 +0200

    Dark mode and profile picture

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
+export const SITE_TITLE = 'diegodelemos';
+export const AUTHOR_FULL_NAME = 'Diego Rodríguez Rodríguez';
+export const AUTHOR_HANDLE = 'diegodelemos';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
