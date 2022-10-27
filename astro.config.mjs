b02934461ecd22714240ec6ae3c77a4995526b72
Diego Rodríguez Rodríguez <diegodelemos@pm.me>
2022-10-27 23:55:38 +0200

    Missing astro extenions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://diegodelemos.me/',
-})
+  site: 'https://diegodelemos.me/',
+  integrations: [mdx(), image(), tailwind(), sitemap()]
+});
